ca6e6947b07811d8f7b4cb14515daf89fdd58c7e
Riddhi-rid <115333978+Riddhi-rid@users.noreply.github.com>
2024-12-21 11:42:29 +0530

    initial commit

---
+++ app/src/main/java/com/example/apiproject/ApiService.java
@@ -0,0 +1,12 @@
+package com.example.apiproject;
+
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+public interface ApiService {
+
+    @GET("posts")
+    Call<List<Post>> getPosts();
+}
